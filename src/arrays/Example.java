23048a3355d0d64f69c884651abfaf3438ab4765
Amra Sezairi <amra.sezairi@hotmail.com>
2020-05-01 18:47:52 -0400

    Example Algorithms

--- src/arrays/Example.java
+++ src/arrays/Example.java
@@ -1,17 +1,38 @@
 package arrays;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Example {
     public static void main(String[] args) {
-        int[] array = {12,56,76,89,100,343,21,234};
-        System.out.println(missingElement(array));
-        System.out.println(searchElementInArray(array,5));
-        System.out.println(searchSecondHighestNumber(array));
+//        int[] array = {12,56,76,89,100,343,21,234};
+//        System.out.println(missingElement(array));
+//        System.out.println(searchElementInArray(array,5));
+//        System.out.println(searchSecondHighestNumber(array));
+//
+//        int array2[] = new int[]{20, 40, 20, 40, 20, 30, 30, 50, 20, 40, 40, 20};
+//        System.out.println("Number which occurs odd number of times is : "+ getOddTimesElement(array2));
+//
+//        smallestAndLargestNumber(array);
+//        findPairWithMinSum(array);
+//
+//
+//        int arrayDiff[]={-40,-5,1,3,6,7,8,20};
+//        findSumOfPairNearestToXElement(arrayDiff,3);
+//        findPairsEqualsToX(arrayDiff,15);
 
-        int array2[] = new int[]{20, 40, 20, 40, 20, 30, 30, 50, 20, 40, 40, 20};
-        System.out.println("Number which occurs odd number of times is : "+ getOddTimesElement(array2));
 
-        smallestAndLargestNumber(array);
-        findPairWithMinSum(array);
+        int arr0s1s[] = {0,1,0,0,1,1,1,0,1};
+        System.out.println("Original Array: ");
+        for (int i = 0; i < arr0s1s.length; i++) {
+            System.out.print(arr0s1s[i]+" ");
+        }
+        arr0s1s=separate0s1s(arr0s1s);
+        System.out.println("\n===========================");
+        System.out.println("\nArray after separating 0's and 1's : ");
+        for (int i = 0; i < arr0s1s.length; i++) {
+            System.out.print(arr0s1s[i]+" ");
+        }
     }
 
     public static int missingElement(int[] array){
@@ -68,8 +89,7 @@ public class Example {
         return second;
     }
 
-    public static int getOddTimesElement(int ar[])
-    {
+    public static int getOddTimesElement(int ar[]) {
         int i;
         int result = 0;
         for (i = 0; i<ar.length ; i++) {
@@ -114,4 +134,66 @@ public class Example {
         System.out.println("The pair whose sum is closest to zero: " + arr[pars1Index] + " ," + arr[pars2Index]);
     }
 
+    public static void findSumOfPairNearestToXElement(int[] array,int x) {
+        if (array.length < 2) {
+            return;
+        }
+        int minimumDiff = (array[0] + array[1] - x);
+        int element1PairIndex = 0;
+        int element2PairIndex = 1;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                int tempDiff = array[i] + array[j] - x;
+                if (Math.abs(tempDiff) < Math.abs(minimumDiff)) {
+                    element1PairIndex = i;
+                    element2PairIndex = j;
+                    minimumDiff = tempDiff;
+
+                }
+            }
+
+        }
+
+        System.out.println(" The pair whose sum is closest to X using brute force method: " + array[element1PairIndex] + " ," + array[element2PairIndex]);
+    }
+
+    public static void findPairsEqualsToX(int[] arr,int x){
+        if(arr.length<2){
+            return;
+        }
+        Arrays.sort(arr);
+        System.out.println("The pair sum whose sum is equals to X: " + x);
+        int l = 0;
+        int r = arr.length-1;
+        while(l<r){
+            int currentSum = arr[l] + arr[r];
+            if(currentSum == x){
+                System.out.println(arr[l] + " " + arr[r]);
+                l++;
+                r--;
+            }
+            else if(arr[l]+ arr[r]<x){
+                l++;
+            }
+            else {
+                r--;
+            }
+        }
+    }
+
+    public static int[] separate0s1s(int[] arr){
+        int count=0;
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i]==0){
+                count ++;
+            }
+        }
+        for (int i = 0; i <count ; i++) {
+            arr[i] = 0;
+        }
+        for (int i = count; i < arr.length; i++) {
+            arr[i]=1;
+        }
+        return arr;
+    }
 }
