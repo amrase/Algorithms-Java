7b5f0c3216ca417cef557de92aa0a4ca1e31b51e
Amra Sezairi <amra.sezairi@hotmail.com>
2020-05-01 16:14:39 -0400

    Practise

--- src/arrays/Example.java
+++ src/arrays/Example.java
@@ -2,9 +2,16 @@ package arrays;
 
 public class Example {
     public static void main(String[] args) {
-        int[] array = {1,2,3,5};
+        int[] array = {12,56,76,89,100,343,21,234};
         System.out.println(missingElement(array));
         System.out.println(searchElementInArray(array,5));
+        System.out.println(searchSecondHighestNumber(array));
+
+        int array2[] = new int[]{20, 40, 20, 40, 20, 30, 30, 50, 20, 40, 40, 20};
+        System.out.println("Number which occurs odd number of times is : "+ getOddTimesElement(array2));
+
+        smallestAndLargestNumber(array);
+        findPairWithMinSum(array);
     }
 
     public static int missingElement(int[] array){
@@ -45,4 +52,66 @@ public class Example {
         }
         return -1;
     }
+
+    public static int searchSecondHighestNumber(int[] array){
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i]>first){
+                second = first;
+                first = array[i];
+            }
+            else if (array[i]>second && first!= array[i]){
+                second = array[i];
+            }
+        }
+        return second;
+    }
+
+    public static int getOddTimesElement(int ar[])
+    {
+        int i;
+        int result = 0;
+        for (i = 0; i<ar.length ; i++) {
+            result = result ^ ar[i];
+        }
+        return result;
+    }
+
+    public static void smallestAndLargestNumber(int[] array){
+        int smallest = array[0];
+        int largest = array[0];
+        for(int i=0;i<array.length;i++){
+            if(array[i] > largest){
+                largest =array[i];
+            }
+            else if(array[i]<smallest){
+                smallest = array[i];
+            }
+        }
+        System.out.println("Smallest number is " + smallest);
+        System.out.println("Largest number is " + largest);
+
+    }
+
+    public static void findPairWithMinSum(int arr[]){
+        if(arr.length<2){
+            return;
+        }
+        int minimumSum = arr[0] + arr[1];
+        int pars1Index = 0;
+        int pars2Index = 1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                int tempSum = arr[i] + arr[j];
+                if(Math.abs(tempSum) < Math.abs(minimumSum)){
+                    pars1Index=i;
+                    pars2Index=j;
+                    minimumSum = tempSum;
+                }
+            }
+        }
+        System.out.println("The pair whose sum is closest to zero: " + arr[pars1Index] + " ," + arr[pars2Index]);
+    }
+
 }
